5106696e9aabc30c82b0fc53a0afe382518fb461
Ndamulelo30 <141257791+Ndamulelo30@users.noreply.github.com>
2024-02-15 10:49:28 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/javacomments/App.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javacomments;
+
+/**
+ *
+ * @author Sanele
+ */
+public class App {
+
+    public static void main(String[] args) {
+        //This is a comment
+        System.out.println("Hello world, whats good?");
+    }
+}
